a4950ff98211f594e33192062c0c25413a7b0049
Paul Maruhn <paulmaruhn@gmail.com>
2017-08-28 04:25:50 +0200

    add proxer datatypes

---
+++ src/prelude.rs
@@ -0,0 +1,90 @@
+#![allow(dead_code)]
+#![deny(unused_imports)]
+#![allow(warnings)]
+#![allow(unused)]
+
+
+use std::string::ToString;
+
+
+#[derive(Debug, Clone)]
+pub struct UserID(u64);
+
+pub trait IsUserID<T> {
+    fn as_userid(self) -> UserID;
+}
+
+
+impl IsUserID<i32> for u64 {
+    fn as_userid(self) -> UserID {
+        UserID(self as u64)
+    }
+}
+
+impl From<i32> for UserID {
+    fn from(x: i32) -> Self {
+        UserID(x as u64)
+    }
+}
+
+impl From<u32> for UserID {
+    fn from(x: u32) -> Self {
+        UserID(x as u64)
+    }
+}
+
+impl From<u64> for UserID {
+    fn from(x: u64) -> Self {
+        UserID(x as u64)
+    }
+}
+
+
+// impl ToString for UserID {
+//     fn to_string(&self) -> String {
+//         self.to_string()
+//     }
+// }
+
+
+
+
+#[derive(Debug, Clone)]
+pub struct InfoID(u64);
+
+pub trait IsInfoID {
+    fn as_infoid(self) -> InfoID;
+}
+
+
+impl From<i32> for InfoID {
+    fn from(x: i32) -> Self {
+        InfoID(x as u64)
+    }
+}
+
+impl From<i64> for InfoID {
+    fn from(x: i64) -> Self {
+        InfoID(x as u64)
+    }
+}
+
+impl From<u32> for InfoID {
+    fn from(x: u32) -> Self {
+        InfoID(x as u64)
+    }
+}
+
+impl From<u64> for InfoID {
+    fn from(x: u64) -> Self {
+        InfoID(x as u64)
+    }
+}
+
+
+
+impl ToString for InfoID {
+    fn to_string(&self) -> String {
+        self.0.to_string()
+    }
+}
