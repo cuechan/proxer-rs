c9dde3835b55f8d14dc3d741846d33606477626f
Paul Maruhn <paulmaruhn@gmail.com>
2017-10-07 16:33:32 +0200

    add list api specification

---
+++ src/api/list.rs
@@ -0,0 +1,38 @@
+#![allow(dead_code)]
+#![allow(unused)]
+#![warn(missing_docs)]
+
+
+use ApiResponse;
+use chrono;
+use error;
+use error::api;
+use prelude::*;
+use Proxer;
+use request;
+use request::info::*;
+use reqwest;
+use reqwest::IntoUrl;
+use response::info;
+use serde_derive;
+use serde_json;
+use serde_json::Value;
+use std;
+use std::collections::HashMap;
+use std::ops::Deref;
+use std::process::exit;
+use std::rc::Rc;
+use std::thread;
+use std::time;
+
+
+pub struct List<'a> {
+    pub proxer: Proxer<'a>
+}
+
+
+impl<'a> List<'a> {
+    fn get_entry_list(self) -> request::list::GetEntryList<'a> {
+        request::list::GetEntryList
+    }
+}
