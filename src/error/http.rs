47c96e4de3d3e0e5a136664b2a76c53e5d967581
Paul Maruhn <paulmaruhn@gmail.com>
2017-12-11 20:49:22 +0100

    add http error type

---
+++ src/error/http.rs
@@ -0,0 +1,34 @@
+use std::fmt;
+use reqwest;
+
+
+
+
+
+#[derive(Debug)]
+pub struct Http {
+	err: reqwest::Error,
+}
+
+
+
+
+impl Http {
+	pub fn new(error: reqwest::Error) -> Self {
+		Self {
+			err: error
+		}
+	}
+}
+
+
+
+
+
+
+impl fmt::Display for Http {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result
+	{
+		write!(f, "http error")
+	}
+}
