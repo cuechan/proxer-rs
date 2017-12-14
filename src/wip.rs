62bed55e7d11da1e4f2a69bba1b9ffedf0ab0689
Paul Maruhn <paulmaruhn@gmail.com>
2017-12-14 01:37:22 +0100

    Revert "add test"
    This reverts commit cdd7d131d5019f14478c6cac7487907f6a5be335.

--- src/wip.rs
+++ src/wip.rs
@@ -3,71 +3,14 @@ extern crate proxer;
 use proxer::Client;
 use proxer::parameter;
 use proxer::Endpoint;
-use std::error::Error;
-use proxer::Pager;
-use proxer::Pageable;
-use std::ops::Add;
 
 
 fn main() {
-    get_comments();
-}
 
-
-
-
-fn get_fullentry() {
     let prxr = Client::with_env_key("PROXER_API_KEY").unwrap();
 
 
-    let req = prxr
-        .api()
-        .info()
-        .get_fullentry(parameter::InfoGetFullEntry {id: 53});
-
-
-    let res = prxr.execute(req);
+    let res = prxr.api().info().get_fullentry(parameter::InfoGetFullEntry {id: 53}).send();
 
     eprintln!("{:#?}", res);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-fn get_comments() {
-    let prxr = Client::with_env_key("PROXER_API_KEY").unwrap();
-
-    let req = prxr.api().info().get_comments(parameter::InfoGetComments {
-        id: 53,
-        p: None,
-        limit: None,
-        sort: None
-    });
-
-
-    let pager = req.pager(prxr);
-
-    let mut counter = 0;
-
-    for comment in pager {
-        counter = counter.add(1);
-        let comment = comment.unwrap();
-
-        println!("{}", counter);
-        println!("{}", comment.username);
-        println!("{}", comment.episode);
-        println!("{}", comment.rating);
-
-        println!();
-
-    }
 }
